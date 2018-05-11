d4fd48173a532471c157071b30a2eb0ba8d89adb
Yassin <necroxx206@gmail.com>
2018-05-11 15:08:13 -0400

    Initial check-in of module findArmStrongNumbers

---
+++ findArmStrongNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace findArmStrongNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double i = 0; i < 999; i++ )
+            {
+                double[] nums = new double[i.ToString().Length];
+                string numbersInLetter = i.ToString();
+
+                for (int x = 0; x < nums.Length; x++ )
+                {
+                    nums[x] = numbersInLetter[x] - 48;
+                }
+                double total = 0;
+                for (int x = 0; x < nums.Length; x++)
+                {
+                    total += Math.Pow(nums[x], 3);
+                }
+
+                double finalNum = Convert.ToDouble(numbersInLetter);
+
+                if(finalNum== total)
+                {
+                    Console.WriteLine(total + " Is an ArmStrong Number");
+                }
+
+
+            }
+        }
+    }
+}
